689b438ba2e9200bcb6304de18563ab6414dae86
Mariu Andrade Veloz <maanvelo@espol.edu.ec>
2025-01-03 10:51:17 -0500

    cambios

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,56 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+
+import {  
+  /* Importe los componentes de la UI */
+  IonCard, IonCardHeader, IonCardTitle, IonCardContent,
+  IonSelect, IonSelectOption, IonTextarea,IonButton,
+  IonList, IonItem, IonLabel,
+  IonHeader, IonToolbar, IonTitle, IonContent 
+} from '@ionic/angular/standalone';
+
+/* Importe el módulo para formularios reactivos */
+import { ReactiveFormsModule } from '@angular/forms';
+/* Importe los constructores del formulario */
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+/* Importe el servicio */
+import { ProviderService } from '../services/provider.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent,          
+  /* Registre los componentes de la UI */
+  IonCard, IonCardHeader, IonCardTitle, IonCardContent,
+  IonSelect, IonSelectOption, IonTextarea,IonButton,
+  IonList, IonItem, IonLabel, ReactiveFormsModule,
+  ]
 })
 export class Tab2Page {
 
-  constructor() {}
+    /* Instancie un formulario */
+    myForm: FormGroup = new FormGroup({
+      score: new FormControl("", Validators.required),
+      opinion: new FormControl("", Validators.required)
+    });
 
+    /* Nombre de la colección */
+    collectionName = 'reviews';
+
+     /* Arreglo con datos locales */
+     dataList: any[] = [];
+
+    /* Inyecte la dependencia a Firestore */
+    constructor(private providerService: ProviderService) { }
+    
+
+     /* El método onSubmit para enviar los datos del formulario mediante el servicio */
+    onSubmit() {
+      this.providerService.createDocument(this.collectionName, this.myForm.value).then(() => {
+          this.myForm.reset()
+    });
+  }
 }
---
+++ src/credentials.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCo1WCohY67O1Couwl2v_b8iM5YzvwuSIA",
+    authDomain: "hibrida-e05df.firebaseapp.com",
+    projectId: "hibrida-e05df",
+    storageBucket: "hibrida-e05df.firebasestorage.app",
+    messagingSenderId: "816717021018",
+    appId: "1:816717021018:web:46989db4cf8bbdda433d63",
+    measurementId: "G-E1651SX7QY"
+};
